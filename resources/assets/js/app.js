c40ecdf151089621d81d72d2d2858f0d99ebec94
jvaroglez <josevaroglez01@gmail.com>
2023-04-20 17:46:25 -0600

    first commit

---
+++ resources/assets/js/app.js
@@ -0,0 +1,14 @@
+require('./bootstrap');
+
+window.Vue = require('vue');
+
+import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
+
+Vue.use(BootstrapVue);
+
+Vue.component('example-component', require('./components/ExampleComponent.vue'));
+
+const app = new Vue({
+    el: '#app'
+});
