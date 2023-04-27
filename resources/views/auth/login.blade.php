10ef6135e8e825825ab6ed1a8e5effed4d291169
jvaroglez <josevaroglez01@gmail.com>
2023-04-27 13:45:36 -0600

    Login UI advances

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,48 +1,46 @@
 @extends('layouts.app')
 
 @section('content')
-<b-container>
-    <b-row align-h="center">
-        <b-col cols="8">
-            <b-card title="Inicio de Sesión">
-                <b-alert show>
-                    Ingresa tus datos weee
-                </b-alert>
-                <p class="card-text">Header and footer using props.</p>
+    <b-container>
+        <b-row align-h="center">
+            <b-col cols="8">
+                <b-card title="Inicio de Sesión">
+                    <b-alert show>
+                        Ingresa tus datos
+                    </b-alert>
 
+                    <b-form method="POST" action="{{ route('login') }}">
+                        {{ csrf_field() }}
 
-                <b-form method="POST" action="{{ route('login') }}">
-                    {{ csrf_field() }}
+                        <b-form-group
+                                label="Email:"
+                                label-for="email"
+                                description="We'll never share your email with anyone else.">
+                            <b-form-input
+                                    type="email"
+                                    id="email"
+                                    name="email"
+                                    value="{{ old('email') }}" required autofocus
+                                    placeholder="hola@email.com">
+                            </b-form-input>
+                        </b-form-group>
 
-                    <b-form-group
-                            label="Email:"
-                            label-for="email"
-                            description="We'll never share your email with anyone else.">
-                        <b-form-input
-                                type="email"
-                                id="email"
-                                name="email"
-                                value="{{ old('email') }}" required autofocus
-                                placeholder="hola@email.com">
-                        </b-form-input>
-                    </b-form-group>
+                        <b-form-group
+                                label="Password:"
+                                label-for="password">
+                            <b-form-input
+                                    type="password"
+                                    id="password"
+                                    name="password"
+                                    value="{{ old('password') }}" required>
+                            </b-form-input>
+                        </b-form-group>
 
-                    <b-form-group
-                            label="Password:"
-                            label-for="password">
-                        <b-form-input
-                                type="password"
-                                id="password"
-                                name="password"
-                                value="{{ old('password') }}" required>
-                        </b-form-input>
-                    </b-form-group>
-
-                    <b-form-group>
-                        <b-form-checkbox name="remember" {{ old('remember') ? 'checked="true"' : '' }}>
-                            Remember
-                        </b-form-checkbox>
-                    </b-form-group>
+                        <b-form-group>
+                            <b-form-checkbox name="remember" {{ old('remember') ? 'checked="true"' : '' }}>
+                                Remember
+                            </b-form-checkbox>
+                        </b-form-group>
 
                         <b-button type="submit" variant="primary">
                             Login
@@ -51,10 +49,10 @@
                         <b-button href="{{ route('password.request') }}" variant="link">
                             Forgot Your Password?
                         </b-button>
-                </b-form>
-            </b-card>
+                    </b-form>
+                </b-card>
 
-        </b-col>
-    </b-row>
-</b-container>
+            </b-col>
+        </b-row>
+    </b-container>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,6 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
@@ -12,67 +11,32 @@
 
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-default navbar-static-top">
-            <div class="container">
-                <div class="navbar-header">
+<div id="app">
+    <b-navbar toggleable="sm" type="dark" variant="primary">
+        <b-navbar-toggle target="nav-text-collapse"></b-navbar-toggle>
 
-                    <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse" aria-expanded="false">
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
+        <b-navbar-brand href="{{ url('/') }}">{{ config('app.name', 'Laravel') }}</b-navbar-brand>
 
-                    <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
-                    </a>
-                </div>
+        <b-collapse id="nav-text-collapse" is-nav>
+            <b-navbar-nav class="ml-auto">
+                @guest
+                    <b-nav-item href="{{ route('login') }}">Login</b-nav-item>
+                    <b-nav-item href="{{ route('register') }}">Register</b-nav-item>
+                @else
+                    <!-- Navbar dropdowns -->
+                    <b-nav-item-dropdown text="Username" right>
+                        <b-dropdown-item href="#">Logout</b-dropdown-item>
+                    </b-nav-item-dropdown>
+                @endguest
+            </b-navbar-nav>
+        </b-collapse>
 
-                <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
-                        &nbsp;
-                    </ul>
+    </b-navbar>
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="nav navbar-nav navbar-right">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li><a href="{{ route('login') }}">Login</a></li>
-                            <li><a href="{{ route('register') }}">Register</a></li>
-                        @else
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
+    @yield('content')
+</div>
 
-                                <ul class="dropdown-menu">
-                                    <li>
-                                        <a href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            Logout
-                                        </a>
-
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                </ul>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
-        @yield('content')
-    </div>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}"></script>
+<!-- Scripts -->
+<script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
